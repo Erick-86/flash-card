a61d1d08c96dcf6adaa9a3ac30103022cfd07435
Freeman <eric.asante.freeman@gmail.com>
2025-12-17 14:48:35 +0000

    added cards to canvas

--- main.py
+++ main.py
@@ -9,4 +9,13 @@ root.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 canvas = Canvas(width=800, height=600, bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
+# CARDS
+# Front card
+front_card = PhotoImage(file="./images/card_front.png")
+
+# Back card
+back_card = PhotoImage(file="./images/card_back.png")
+
+card = canvas.create_image(400, 263, image=back_card)
+
 root.mainloop()
