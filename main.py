791d6c56fa35b4d76b3cb23013bf68ffc1155df4
Freeman <eric.asante.freeman@gmail.com>
2025-12-17 14:40:20 +0000

    canvas bg color and grid layout

--- main.py
+++ main.py
@@ -6,4 +6,7 @@ BACKGROUND_COLOR = "#B1DDC6"
 root.title("Flash Card")
 root.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 
+canvas = Canvas(width=800, height=600, bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
 root.mainloop()
